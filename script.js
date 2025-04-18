5e9c2d85d3b7ceaeec5adf41b7106f71b9cda855
suryadizhang <suryadizhang86@gmail.com>
2025-04-18 14:36:22 -0700

    done

--- script.js
+++ script.js
@@ -13,5 +13,37 @@ document.getElementById("search-button").addEventListener("click",async()=>
         container.innerHTML = "<p class='error'>Please enter a Pokémon name or ID</p>";
         return;
     }
+    try {
+        //fetch the Pokémon data from the API and make the input data to lower case//
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${input.toLowerCase()}`);
+        //awaait for the response to be returned//
+        if(!response.ok)
+        {
+            //if the response is not ok, display an error message//
+            container.innerHTML = "<p class='error'>Pokémon not found</p>";
+            return;
+        }
+        const data = await response.json();
+        displayPokemon(data); //call the function to display the Pokémon data//
+    } catch (error) {
+        //if there is an error, display an error message//
+        container.innerHTML = "<p class='error'>pokemon not found!</p>";
+    }
+    //displayPokemon(data); //call the function to display the Pokémon data//
+    function displayPokemon(data) {
+        const container = document.getElementById("pokedex-container");
+        const pokenonName = data.name.charAt (0).toUpperCase() + data.name.slice(1); //capitalize the first letter of the Pokémon name//
+        const pokemonImage = data.sprites.front_default; //get the Pokémon image from the API//
+        const pokemonTypes = data.types.map(type => type.type.name).join(", "); //get the Pokémon types from the API//
+        const pokemonAbilities = data.abilities.map(ability => ability.ability.name).join(", "); //get the Pokémon abilities from the API//
+        const pokemonStats = data.stats.map(stat => `${stat.stat.name}: ${stat.base_stat}`).join(", "); //get the Pokémon stats from the API//
+
+        container.innerHTML = ` 
+        <h2>${pokenonName}</h2>
+        <img src="${pokemonImage}" alt="${pokenonName}">
+        <p><strong>Types:</strong> ${pokemonTypes}</p>
+        <p><strong>Abilities:</strong> ${pokemonAbilities}</p>
+        <p><strong>Stats:</strong> ${pokemonStats}</p>
+        `; //display the Pokémon data in the container//
 
 }
